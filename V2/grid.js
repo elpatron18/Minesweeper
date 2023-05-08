946646fae7a8da165338d9653b8585fe206374c4
el patron <60261111+elpatron18@users.noreply.github.com>
2023-05-08 08:36:56 +0200

    0 Rekursion

--- V2/grid.js
+++ V2/grid.js
@@ -16,7 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
             this.zeile = zeile;
             this.spalte = spalte;
             this.bombe = false;
-            this.umkreis = null;
+            this.visible = false;
+            this.umkreisZahl = null;
 
             // Erstelle eine neue Div
             this.div = document.createElement('div');
@@ -29,23 +30,81 @@ document.addEventListener('DOMContentLoaded', () => {
 
             meinGrid.appendChild(this.div)
         }
+        updateZahl() {
+            if (this.bombe) return;
+            let summe = 0;
+
+            this.getMyUmfeld().forEach((element) => {
+                if (element.bombe) summe++;
+            })
+
+            this.umkreisZahl = summe;
+        }
+
+        aufdecken() {
+            if (!this.visible) this.visible = true;
+
+            if (this.bombe) this.div.style.backgroundImage = "url('images/Bomb.png')";
+             else {
+                // Wenn null ist deck alle Werte drum rum auf
+                // Wenn != 0 dann zeige die Zahl an
+                    if (this.umkreisZahl === 0) {
+                        this.div.style.backgroundColor = "lightgrey";
+                        this.visible = true;
+                        this.getMyUmfeld().forEach(element => {
+                            if (!element.visible) element.aufdecken();
+                        })
+                    }
+                    else {
+                        this.div.innerHTML = this.umkreisZahl;
+                        this.visible = true;
+                    }
+            }
+        }
+
+        getMyUmfeld() {
+            let umfeld = [];
+
+            let obenRand = this.zeile === 0;
+            let untenRand = this.zeile === zeilenAnzahl - 1;
+            let linksRand = this.spalte === 0;
+            let rechtsRand = this.spalte === spaltenAnzahl - 1;
+
+            let rOffStart = -1, rOffStop = 1, cOffStart = -1, cOffStop = 1;
+
+            if (obenRand) rOffStart = 0;
+            if (untenRand) rOffStop = 0;
+            if (linksRand) cOffStart = 0;
+            if (rechtsRand) cOffStop = 0;
+
+            for (let rOff = rOffStart; rOff <= rOffStop; rOff++) {
+                for (let cOff = cOffStart; cOff <= cOffStop; cOff++) {
+                    umfeld.push(karte[this.zeile + rOff][this.spalte + cOff]);
+                }
+            }
+
+            return umfeld;
+        }
+
     }
+
+
 
     function erstelleKarte() {
         // erstelle ein leeres 2D-Array
         let karte = new Array(zeilenAnzahl);
-        for (let z = 0; i < zeilenAnzahl; i++) {
+        for (let z = 0; z < zeilenAnzahl; z++) {
             karte[z] = new Array(spaltenAnzahl);
             for (let s = 0; s < spaltenAnzahl; s++) {
                 karte[z][s] = new Zelle(z, s);
             }
         }
 
-        //Bomben erstellen
+        // Bomben erstellen
         let bomben = 0;
         while (bomben < anzahlBomben) {
-            let b = Math.floor(Math.random() * breite);
-            let h = Math.floor(Math.random() * hoehe);
+            let b = Math.floor(Math.random() * spaltenAnzahl);
+            let h = Math.floor(Math.random() * zeilenAnzahl);
 
             if (!karte[h][b].bombe) {
                 karte[h][b].bombe = true;
@@ -54,5 +113,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         return karte;
     }
+    let karte = erstelleKarte();
+
+    karte.forEach((z) => z.forEach((s) => s.updateZahl()))
+
+    karte.forEach((z) => z.forEach((s) => {
+        s.div.addEventListener("click", () => {
+            s.aufdecken();
+        });
+    }));
+
 
 })
